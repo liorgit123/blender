4ec1a9b9c49838eef8661e4729c9fba664bc5f4b
liorgit123 <liorta@gmail.com>
2026-09-01 21:46:24 +0300

    Update fireworks.js

--- js/fireworks.js
+++ js/fireworks.js
@@ -50,7 +50,7 @@ function triggerFireworks(intensity = "high") {
     // If low intensity, force red. Otherwise use random colors
     particle.style.backgroundColor = intensity === "high"
         ? colors[Math.floor(Math.random() * colors.length)]
-        : "#FF0000";
+        : "#0FFF50";
 
       particle.style.left = `${x}px`;
       particle.style.top = `${y}px`;
